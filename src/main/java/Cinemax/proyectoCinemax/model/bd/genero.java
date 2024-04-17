08f408b475113629eb7f8d3de83c8e1643b40725
LuisRoj <luisrojas150520@gmail.com>
2024-04-16 21:44:18 -0500

    Mapeo de objetos pelicula y genero

--- src/main/java/Cinemax/proyectoCinemax/model/bd/genero.java
+++ src/main/java/Cinemax/proyectoCinemax/model/bd/genero.java
@@ -1,4 +1,15 @@
 package Cinemax.proyectoCinemax.model.bd;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Peliculas")
 public class genero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idgenero; // Cambiar el nombre del campo
+
+    private String nom_genero;
 }
