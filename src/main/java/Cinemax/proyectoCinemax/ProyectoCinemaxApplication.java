db376fd7e4fdbd6290a12e6170f98d3e98a7d198
LuisRoj <luisrojas150520@gmail.com>
2024-04-16 21:09:00 -0500

    Estructura de proyecto

---
+++ src/main/java/Cinemax/proyectoCinemax/ProyectoCinemaxApplication.java
@@ -0,0 +1,13 @@
+package Cinemax.proyectoCinemax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCinemaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCinemaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cinemax/proyectoCinemax/ProyectoCinemaxApplicationTests.java
@@ -0,0 +1,13 @@
+package Cinemax.proyectoCinemax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCinemaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
