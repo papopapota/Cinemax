6a2be8119cce9bfa7c0475ca974b28c6e7e491ab
LuisRoj <luisrojas150520@gmail.com>
2024-04-18 00:16:57 -0500

    funciones javascript

--- src/main/resources/static/js/web/backoffice/pelicula/formpelicula.js
+++ src/main/resources/static/js/web/backoffice/pelicula/formpelicula.js
@@ -1,4 +1,13 @@
 $(document).on("click", "#btnagregar", function(){
+    $("#txtnompelicula").val("");
+    $("#txtdescpelicula").val("");
+    $("#hddpelicod").val("0");
     $("#modalpelicula").modal("show");
 });
 
+$(document).on("click", ".btnactualizar", function(){
+    $("#txtnompelicula").val($(this).attr("data-pelititulo"));
+    $("#txtdescpelicula").val($(this).attr("data-pelidescrip"));
+    $("#hddpelicod").val($(this).attr("data-pelicod"));
+    $("#modalpelicula").modal("show");
+});
